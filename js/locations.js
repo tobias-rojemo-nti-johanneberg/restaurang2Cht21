fae1a161d92d62f781c916150883b081e653cdb7
tobias-rojemo-nti-johanneberg <tobias.rojemo@elev.ga.ntig.se>
2021-12-22 02:29:13 +0100

    Added logo Added menu items Minor fixes Cleaning of code

--- js/locations.js
+++ js/locations.js
@@ -1,25 +1,26 @@
-const main = document.getElementById('location-wrapper');
+const wrapper = document.getElementById('location-wrapper');
+const buttons = [];
 const locations = [];
 
-for (let i = 0; i < main.children.length; i++) {
-  locations.push(main.children[i]);
+for (let i = 0; i < wrapper.children.length; i++) {
+  buttons.push(document.getElementById(`scroll-to-${i+1}`));
+  buttons[i].addEventListener('click', () => {moveToLocation(i);})
+}
+
+for (let i = 0; i < wrapper.children.length; i++) {
+  locations.push(wrapper.children[i]);
 }
 
 let currentLocation = 0;
-locations[0].children[0].hidden = false;
-locations[0].children[1].children[0].hidden = false;
-locations[0].children[1].children[1].hidden = false;
 
 function nextLocation() {
-  locations[currentLocation%locations.length].children[0].hidden = true;
-  locations[currentLocation%locations.length].children[1].children[0].hidden = true;
-  locations[currentLocation%locations.length].children[1].children[1].hidden = true;
-
   currentLocation++;
-  
-  locations[currentLocation%locations.length].children[0].hidden = false;
-  locations[currentLocation%locations.length].children[1].children[0].hidden = false;
-  locations[currentLocation%locations.length].children[1].children[1].hidden = false;
+  wrapper.style.marginLeft = `-${currentLocation%5*100}%`;
 }
 
-//setInterval(nextLocation, 10000);
+function moveToLocation(location) {
+  currentLocation = location;
+  wrapper.style.marginLeft = `-${currentLocation%5*100}%`;
+}
+
+setInterval(nextLocation, 10000);
